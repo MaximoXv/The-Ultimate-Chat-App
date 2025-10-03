57d0f9e39ec053ca87831376035179c0fe102d4b
MaximoXv <maximoibarra164@gmail.com>
2025-10-02 22:17:13 -0300

    Feat: implement font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 function App() {
   return (
     <>
-      <h1 className="text-blue-400">hola mundo</h1>
+      <div className="w-screen min-h-screen font-bold">
+        <div className="w-full flex justify-between items-center gap-1.5 p-2">
+          <h1 className="text-blue-400">The Ultimate Chat</h1>
+          <div className="">
+            <span className="material-symbols-outlined text-blue-400">
+              more_vert
+            </span>
+          </div>
+        </div>
+        <div className="w-full p-2">
+          <div className="w-full flex gap-1.5 p-2 bg-gray-400 rounded-2xl">
+            <input type="text" placeholder="Buscar contacto..." />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
